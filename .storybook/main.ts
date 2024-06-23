2e5c0f29ccfb99bff65d4ccce6c3afa649a42ab4
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-06-24 00:33:49 +0900

    chore: storybook 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,25 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  features: {
-    storyStoreV7: true,
-  },
+
   docs: {
-    docsMode: true,
-    autodocs: true,
+    docsMode: true
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 } satisfies StorybookConfig;
